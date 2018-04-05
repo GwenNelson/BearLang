1d697bcb29930559de77090baf40a2c339afc750
Gareth Nelson <gareth@garethnelson.com>
2018-04-05 21:32:02 +0100

    start of real REPL

--- bli/src/main.c
+++ bli/src/main.c
@@ -8,10 +8,12 @@
 #include <readline/readline.h>
 
 int main(int argc, char** argv) {
+    bl_init();
     printf("BearLang Version 0.WHATEVER\n\n"); // TODO: add versioning
     for(;;) {
         char* input_line = readline(">>> ");
         bl_ast_node_t* ast = bl_parse_sexp(input_line);
-
+        bl_val_t*      expr = bl_read_ast(ast);
+        printf("%s\n",bl_ser_sexp(expr));
     }
 }
